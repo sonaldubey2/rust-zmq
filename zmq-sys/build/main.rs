bc3f51201ca30006c2af83fb169d6a40d4021bc2
Sonal Dubey <sdubey2@rockwellautomation.com>
2022-09-06 23:25:57 -0400

    Pkg config for macos

--- zmq-sys/build/main.rs
+++ zmq-sys/build/main.rs
@@ -1,3 +1,4 @@
+#[cfg_attr(feature = "vendored", target_os = "macos", path = "pkg_config.rs")]
 #[cfg_attr(feature = "vendored", path = "vendored.rs")]
 #[cfg_attr(not(feature = "vendored"), path = "pkg_config.rs")]
 mod find;
